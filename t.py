f10f7ed0a2aab6c67dd3ff4159bf05688969a2d7
williamyizhu <william.yizhu@live.com>
2017-06-21 16:56:21 +0800

    modify t.py

--- t.py
+++ t.py
@@ -2,24 +2,27 @@ import sys
 import subprocess
 import time
 
-# mongodb = 'mongodb1'
-mongodb = 'mongodbi'
+account = 'real_xh1'
+# account = 'real_eb1'
 
-subprocess.Popen([sys.executable,'live_quote.py','-m','t','-a','real_eb1','-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
+mongodb = 'mongodb1'
+# mongodb = 'mongodbi'
+
+subprocess.Popen([sys.executable,'live_quote.py','-m','t','-a',account,'-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 time.sleep(10)
 
-subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','cffex','-a','real_eb1','-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
-subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','shfe','-a','real_eb1','-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
-subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','dce','-a','real_eb1','-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
-subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','czce','-a','real_eb1','-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','cffex','-a',account,'-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','shfe','-a',account,'-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','dce','-a',account,'-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','futures','-e','czce','-a',account,'-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
-subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','options','-e','dce','-a','real_eb1','-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen([sys.executable,'live_quote.py','-m','q','-p','options','-e','dce','-a',account,'-d',mongodb], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 
 # cd 'Z:\williamyizhu On My Mac\Documents\workspace\PyCtp'
-# python .\live_quote.py -m t -a real_eb2 -d mongodb1
-# python .\live_quote.py -m t -a real_xh -d mongodb1
-# python .\live_quote.py -m t -a real_ht -d mongodb1
-# python .\live_quote.py -m q -p futures -e dce -a real_eb2 -d mongodb1
+# python .\live_quote.py -m t -a real_eb1 -d mongodb1
+# python .\live_quote.py -m t -a real_xh1 -d mongodb1
+# python .\live_quote.py -m t -a real_ht1 -d mongodb1
+# python .\live_quote.py -m q -p futures -e dce -a real_xh1 -d mongodb1
 # python .\live_quote.py -m q -p options -e dce -a real_eb2 -d mongodb1
